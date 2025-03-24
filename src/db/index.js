fcebf04e078f52d63e8eb122f3b03ae4ba191556
ankit <ankitkarapurkar444@gmail.com>
2025-03-24 05:28:55 -0700

    middleware setup

--- src/db/index.js
+++ src/db/index.js
@@ -20,3 +20,9 @@ const connectDB = async () => {
 
 
 export default connectDB;
+/*What is asyncHandler?
+asyncHandler is a higher-order function.
+A higher-order function is a function that takes another function as an argument and possibly returns a new function.
+
+Why is asyncHandler needed?
+In Express.js, if an asynchronous function throws an error, you need to explicitly pass that error to the next function for the Express error-handling middleware to catch it. Without this, your server might crash or not handle the error properly.*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import cors from 'cors';
+import express from "express";
+import cookieParser from 'cookie-parser';
+const app = express();
+app.use(express.json());
 
 dotenv.config({path:'./env'});
-connectDB();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"));
+app.use(cookieParser);
+connectDB()
+.then(()=>{
+    app.listen( process.env.PORT || 8000,()=>{
+        console.log(`the server is running on port ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+console.log("mongo db failed connection",err);
+});
