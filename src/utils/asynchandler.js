afa1dd57b0d0a03f1a3c8412da479e6889ba9c93
ankit <ankitkarapurkar444@gmail.com>
2025-03-24 08:31:06 -0700

    async handler

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,30 @@
+//asynchandler is a higeher order  function//
+
+const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+        .catch((error) => {
+            res.status(error.code || 500).json({
+                success: false,
+                message: error.message
+            });
+        });
+};
+
+/*
+const asyncHandler=(fn)=> async(req,res,next)=>{
+   try{
+  //execute function taken in argument//
+   await fn(req,res,next);
+
+   }catch(error)
+   {
+    res.status(err.code || 500).json({
+        success:false,
+        message:err.message
+    })
+   }
+
+
+};
+
+*/
